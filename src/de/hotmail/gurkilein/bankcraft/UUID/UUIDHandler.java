65cbbe3478a80afdd2237d6ec6641c440ead14a9
Moritz "Gurke" P. <gurkilein@hotmail.de>
2014-05-10 16:38:59 +0200

    2.4.1
    Reduced amount of http uuid requests

--- src/de/hotmail/gurkilein/bankcraft/UUID/UUIDHandler.java
+++ src/de/hotmail/gurkilein/bankcraft/UUID/UUIDHandler.java
@@ -1,6 +1,7 @@
 package de.hotmail.gurkilein.bankcraft.UUID;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -11,6 +12,9 @@ import de.hotmail.gurkilein.bankcraft.Bankcraft;
 
 public class UUIDHandler {
 
+	private Map<String,UUID> uuidMap = new HashMap <String,UUID>();
+	private Map<UUID,String> nameMap = new HashMap <UUID,String>();
+	
 	private Bankcraft bankcraft;
 
 	public UUIDHandler(Bankcraft bankcraft) {
@@ -30,13 +34,19 @@ public class UUIDHandler {
 	public UUID getUUIDwE(String name) throws Exception {
 		Player p = null;
 		if ((p = bankcraft.getServer().getPlayer(name)) != null) {
+			nameMap.put(p.getUniqueId(), name);
+			uuidMap.put(name, p.getUniqueId());
 			return p.getUniqueId();
+		} else if (uuidMap.containsKey(name)) {
+			return uuidMap.get(name);
 		} else {
 			List <String> uL = new ArrayList <String> ();
 			uL.add(name);
 			UUIDFetcher fetcher = new UUIDFetcher(uL);
 			Map<String, UUID> response = null;
 			response = fetcher.call();
+			nameMap.put(response.get(name), name);
+			uuidMap.put(name, response.get(name));
 			return response.get(name);
 		}
 	}
@@ -44,7 +54,11 @@ public class UUIDHandler {
 	public String getName(UUID uuid) {
 		Player p = null;
 		if ((p = bankcraft.getServer().getPlayer(uuid)) != null) {
+			nameMap.put(uuid, p.getName());
+			uuidMap.put(p.getName(), uuid);
 			return p.getName();
+		} else if (nameMap.containsKey(uuid)) {
+			return nameMap.get(uuid);
 		} else {
 			List <UUID> uL = new ArrayList <UUID> ();
 			uL.add(uuid);
@@ -55,6 +69,8 @@ public class UUIDHandler {
 			} catch (Exception e) {
 			e.printStackTrace();
 			}
+			nameMap.put(uuid, response.get(uuid));
+			uuidMap.put(response.get(uuid), uuid);
 			return response.get(uuid);
 		}
 	}
